b5c787a90b9baaa779d75185138f5e2e53cab1d3
protector2dworld <protector2dworld@gmail.com>
2021-06-02 21:35:51 +0300

    homework
    Task 0

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-
-
-//Number('5');
-
-console.log(Number ('5'));
-
-console.log(Number ('asd'));
-
-console.log(typeof + '5');
+const player1 = {
+name: 'Sonya',
+hp: '80',
+img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+weapon: 'katana',
+attack: function() {
+    console.log('Sonya' + 'Fight...');
+}
+};
